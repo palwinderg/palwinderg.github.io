6d2c3b2a57a34bbfce0939c303a6c57c0b51fab4
Palwinder Gill <70971434+palwinderg@users.noreply.github.com>
2023-01-05 16:03:03 -0500

    1.0.0

---
+++ script.js
@@ -0,0 +1,10 @@
+//code for javascript functions used in the html code / website
+
+function onSubmit(){
+    if (document.getElementById('password').value == 'kellytire') {
+        window.location.href = 'kellytire.html';
+     }
+    else{ 
+        alert('Please check your passcode and try again');
+    }
+}
